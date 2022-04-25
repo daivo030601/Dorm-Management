905fe64abe2e03bc2cf5039ab06f1dcf9ec6ba57
Dai Vo <daivobmt01@gmail.com>
2022-04-25 15:14:38 +0700

    adding database

---
+++ quanlyktx.sql
@@ -0,0 +1,270 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quanlyktx
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `IDAccount` varchar(10) NOT NULL,
+  `Username` varchar(40) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Permission` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `apartment`
+--
+
+DROP TABLE IF EXISTS `apartment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `apartment` (
+  `IDApartment` varchar(10) NOT NULL,
+  `NoRoom` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Gender` varchar(4) NOT NULL,
+  `IDEmployee` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `apartment`
+--
+
+LOCK TABLES `apartment` WRITE;
+/*!40000 ALTER TABLE `apartment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `apartment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contract`
+--
+
+DROP TABLE IF EXISTS `contract`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contract` (
+  `IDContract` varchar(10) NOT NULL,
+  `IDRoom` varchar(10) NOT NULL,
+  `IDApartment` varchar(10) NOT NULL,
+  `IDEmployee` varchar(10) NOT NULL,
+  `IDStudent` varchar(10) NOT NULL,
+  `Createday` date DEFAULT NULL,
+  `Startday` datetime DEFAULT NULL,
+  `Endday` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contract`
+--
+
+LOCK TABLES `contract` WRITE;
+/*!40000 ALTER TABLE `contract` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contract` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `electricityandwaterbill`
+--
+
+DROP TABLE IF EXISTS `electricityandwaterbill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `electricityandwaterbill` (
+  `IDEWBill` varchar(10) NOT NULL,
+  `IDEmployee` varchar(10) NOT NULL,
+  `IDRoom` varchar(10) NOT NULL,
+  `IDApartment` varchar(10) NOT NULL,
+  `Createday` date DEFAULT NULL,
+  `ChiSoDauDien` int DEFAULT NULL,
+  `ChiSoCuoiDien` int DEFAULT NULL,
+  `ChiSoDauNuoc` int DEFAULT NULL,
+  `ChiSoCuoiNuoc` int DEFAULT NULL,
+  `Total` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `electricityandwaterbill`
+--
+
+LOCK TABLES `electricityandwaterbill` WRITE;
+/*!40000 ALTER TABLE `electricityandwaterbill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `electricityandwaterbill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `IDEmployee` varchar(10) NOT NULL,
+  `Fullname` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Birthday` date DEFAULT NULL,
+  `Address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Position` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `PhoneNumber` varchar(12) NOT NULL,
+  `IDAccount` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facilities`
+--
+
+DROP TABLE IF EXISTS `facilities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facilities` (
+  `IDFacilities` varchar(10) NOT NULL,
+  `IDRoom` varchar(10) NOT NULL,
+  `IDApartment` varchar(10) NOT NULL,
+  `Status` varchar(40) NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `Amount` int DEFAULT NULL,
+  `Discripition` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facilities`
+--
+
+LOCK TABLES `facilities` WRITE;
+/*!40000 ALTER TABLE `facilities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `facilities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rentbill`
+--
+
+DROP TABLE IF EXISTS `rentbill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rentbill` (
+  `IDTBill` varchar(10) NOT NULL,
+  `IDEmployee` varchar(10) NOT NULL,
+  `IDRoom` varchar(10) NOT NULL,
+  `IDApartment` varchar(10) NOT NULL,
+  `IDStudent` varchar(10) NOT NULL,
+  `Createday` date DEFAULT NULL,
+  `Total` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rentbill`
+--
+
+LOCK TABLES `rentbill` WRITE;
+/*!40000 ALTER TABLE `rentbill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rentbill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `IDRoom` varchar(10) NOT NULL,
+  `IDRapartmentoom` varchar(10) NOT NULL,
+  `IDApartment` varchar(10) NOT NULL,
+  `IDStudent` varchar(10) NOT NULL,
+  `NoStudent` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Status` varchar(40) NOT NULL,
+  `Type` varchar(40) NOT NULL,
+  `RentingPrice` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `IDStudent` varchar(10) NOT NULL,
+  `Fullname` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Birthday` date DEFAULT NULL,
+  `Gender` varchar(4) NOT NULL,
+  `IDCard` varchar(20) NOT NULL,
+  `PhoneNumber` varchar(12) NOT NULL,
+  `University` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Grade` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-25 15:10:33
